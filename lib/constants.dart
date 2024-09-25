e1d90eb7b67ee44f2207b30136a790407e0731ca
Shady <selimokten00@gmail.com>
2024-09-25 19:25:01 +0300

    Files Upload

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,5 +3,5 @@ import 'package:flutter/material.dart';
 const TextStyle kAppBarStyle = TextStyle(
   color: Colors.white,
   fontSize: 25,
-  fontWeight: FontWeight.bold,
-);
+  fontWeight: FontWeight.w100,
+); // rewiew
